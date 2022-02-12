2fb184671574f3e3e6c0373dfdb5fccd608a3f9b
bijinfeng <bijinfeng@bytedance.com>
2022-02-12 16:02:53 +0800

    doc: udpate logo path
    Change-Id: I0a82c0c924c0458f7e476f245f941e5474544a11

--- packages/react-turn/.umirc.ts
+++ packages/react-turn/.umirc.ts
@@ -2,19 +2,21 @@ import { defineConfig, IConfig } from 'dumi';
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = process.env.NODE_ENV === 'production' && process.env.PREVIEW_PR !== 'true';
+const base = isProd ? '/react-turn/' : '/';
 
 export default defineConfig({
   outputPath: 'site',
   exportStatic: isProd ? {} : false,
   title: 'React Turn',
   mode: 'site',
-  favicon: 'http://www.turnjs.com/pics/favicon.png',
-  logo: 'http://www.turnjs.com/pics/favicon.png',
+  favicon: base + 'favicon.png',
+  logo: base + 'favicon.png',
   resolve: {
     passivePreview: true,
   },
+  copy: ['static'],
   hash: isProd,
-  base: isProd ? '/react-turn/' : '/',
+  base,
   publicPath: isProd ? 'https://cdn.jsdelivr.net/gh/bijinfeng/react-turn@gh-pages/' : '/',
   sitemap: {
     hostname: 'https://bijinfeng.github.io/react-turn/',
